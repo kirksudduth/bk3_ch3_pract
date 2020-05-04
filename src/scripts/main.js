2c5ed2d7ffbdfc6fc62ca3f3602d6e5c637ea617
Kirk Sudduth <kirksudduth@gmail.com>
2020-05-04 14:56:46 -0500

    made a function to create an object in main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 console.log("This is main.js");
+
+// Lightning Exercise 1: Write a factory function that
+// creates an object that represents a doctor. The
+// function should accept three arguments.
+
+// Doctor's name
+// Specialty (Oncologist, pediatrician, etc...)
+// Address of practice
+
+const createDoctor = (name, specialty, address) => {
+  return {
+    docName: name,
+    docSpecialty: specialty,
+    docAddress: address,
+  };
+};
+
+const mary = createDoctor("Mary", "Opthomalogist", "300 North Boulevard");
+console.log("createDoctor", mary);
